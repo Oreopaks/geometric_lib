48445662b045e708244ba38d2000afb8cac919a5
Oleg Kryuchkov <466370@niuitmo.ru>
2024-10-02 20:07:51 +0300

    add notes in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,10 @@ import math
 
 
 def area(r):
+    '''Принимает r- радиус окружности и возвращает ее площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает r - радиус окружности и возвращает ее периметр'''
     return 2 * math.pi * r
-
